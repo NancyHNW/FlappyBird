7e2451dd0be6d2ccefeee159f1be85b2e60cccb2
Nancy Wei <nnwi818@aucklanduni.ac.nz>
2024-06-28 21:06:46 +1200

    added flappy bird class + its animation

--- flappy_bird.py
+++ flappy_bird.py
@@ -24,6 +24,33 @@ ground = pygame.image.load("assets/ground.png")
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f"assets/bird{num}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        # handle the animation
+        self.counter += 1
+        flat_cooldown = 5
+
+        if self.counter > flat_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
+
+
+flappy = Bird(100, int(screen_height/2))
+
+bird_group = pygame.sprite.Group()
+bird_group.add(flappy)
 
 
 # create game loop
@@ -37,6 +64,9 @@ while run:
     # scroll background
     screen.blit(ground, (ground_scroll, 768))
     ground_scroll -= scroll_speed
+
+    bird_group.draw(screen)
+    bird_group.update()
 
     if abs(ground_scroll) > 35:
         ground_scroll = 0
